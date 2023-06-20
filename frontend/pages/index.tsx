f650aa32df7c2f97a8f86ac0cb381a1626eb6993
oka311119 <katoko.318@gmail.com>
2023-06-20 18:04:38 +0900

    fix amplify settings

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,44 +1,16 @@
 import { Amplify, API, graphqlOperation } from "aws-amplify";
 import { Flex, withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import config from '../src/aws-exports';
-import {
-  NavBar,
-  FlashCardCollection,
-  CardForm,
-  ChannelForm,
-  CardCreateForm,
-  FAQItemCollection,
-} from "../src/ui-components";
+import awsmobile from '../src/aws-exports';
 import { createCard } from '../src/graphql/mutations';
 import { CardFormInputValues } from "@/src/ui-components/CardForm";
 
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 
 function Home() {
   return (
     <Flex direction="column" className="min-h-screen">
-      <NavBar />
-      <Flex direction="row" className="flex-grow">
-        <FAQItemCollection />
-        <FlashCardCollection />
-        <CardForm />
-        <CardCreateForm
-          // onSubmit={(fields) => {
-          //     // Example function to trim all string inputs
-          //     const updatedFields: CardFormInputValues = {}
-          //     Object.keys(fields).forEach(key => {
-          //         if (typeof fields[key] === 'string') {
-          //             updatedFields[key] = "fields[key]".trim()
-          //         } else {
-          //             updatedFields[key] = fields[key]
-          //         }
-          //     })
-          //     return updatedFields
-          // }}
-        />
-        <ChannelForm />
-      </Flex>
+      <p>Hello world</p>
     </Flex>
   );
 }
